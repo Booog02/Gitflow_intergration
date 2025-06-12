d13894368adf5f72d6afb41bf048469ce3a360cb
BOOGWINDOWS\bukut <bukuthau02@gmail.com>
2025-06-12 18:56:12 +0800

    更新program

--- Git練習01/Git練習01/Program.cs
+++ Git練習01/Git練習01/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Git練習01.Models;
+using Git練習01.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,18 @@ namespace Git練習01
     {
         static void Main(string[] args)
         {
+            LoginModel loginModel = new LoginModel()
+            {
+                Account = "boog",
+                Password = "1234"
+            };
+
+            LoginService loginService = new LoginService();
+
+            LoginResult loginResult = loginService.Login(loginModel);
+
+            Console.WriteLine($"結果:{loginResult.Message}");
+
         }
     }
 }
