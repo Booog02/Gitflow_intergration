4d68b40d010c71d33159780725f1133a86a248a2
BOOGWINDOWS\bukut <bukuthau02@gmail.com>
2025-06-12 17:40:24 +0800

    新增Login Model

--- Git練習01/Git練習01/Models/LoginModel.cs
+++ Git練習01/Git練習01/Models/LoginModel.cs
@@ -1,12 +1,16 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Git練習01.Models
 {
-    public class UserModel
+    internal class LoginModel
     {
         public String Name { get; set; }
         public String Account { get; set; }
         public String Password { get; set; }
-
     }
+
 }
---
+++ Git練習01/Git練習01/Services/LoginService.cs
@@ -0,0 +1,17 @@
+﻿using Git練習01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git練習01.Services
+{
+    internal class LoginService
+    {
+        public bool Login(LoginModel model)
+        {
+            return model.Name == "Boog" && model.Account == "boog02" && model.Password == "1234";
+        }
+    }
+}
