b9e04b907c798a9451c60cdba1472b7e684ba038
周政輝 <leo604leo604@gmail.com>
2025-06-12 17:38:42 +0800

    新增註冊服務

--- Git練習01/Git練習01/Services/RegisterService.cs
+++ Git練習01/Git練習01/Services/RegisterService.cs
@@ -1,7 +1,15 @@
+using Git練習01.Models;
+
 namespace Git練習01.Services
 {
     public class RegisterService
     {
-        
+        public bool UserRegist(RegisterModel model)
+        {
+            UserModel userModel = new UserModel();
+            userModel.Account = model.Account;
+            userModel.Password = model.Password;
+            return true;
+        }
     }
 }
