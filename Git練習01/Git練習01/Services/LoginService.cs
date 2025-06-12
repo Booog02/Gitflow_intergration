f3925eaec7ad88854c69f6180ef5e6e6f1b35435
BOOGWINDOWS\bukut <bukuthau02@gmail.com>
2025-06-12 18:14:48 +0800

    新增登入結果訊息

--- Git練習01/Git練習01/Services/LoginService.cs
+++ Git練習01/Git練習01/Services/LoginService.cs
@@ -9,9 +9,27 @@ namespace Git練習01.Services
 {
     internal class LoginService
     {
-        public bool Login(LoginModel model)
+        //public bool Login(LoginModel model)
+        //{
+        //    return model.Name == "Boog" && model.Account == "boog02" && model.Password == "1234";
+        //}
+
+        public LoginResult Login(LoginModel model)
         {
-            return model.Name == "Boog" && model.Account == "boog02" && model.Password == "1234";
+            if (model.Account == "boog02" && model.Password == "1234")
+            {
+                return new LoginResult
+                {
+                    Success = true,
+                    Message = "登入成功。"
+                };
+            }
+
+            return new LoginResult
+            {
+                Success = false,
+                Message = "帳號或密碼錯誤。"
+            };
         }
     }
 }
