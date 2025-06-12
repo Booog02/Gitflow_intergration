e484fa58fca2f933e8777452affc72f73d06fc36
周政輝 <leo604leo604@gmail.com>
2025-06-12 18:50:30 +0800

    模擬訊息公告

--- Git練習01/Git練習01/Program.cs
+++ Git練習01/Git練習01/Program.cs
@@ -10,6 +10,21 @@ namespace Git練習01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("歡迎來到 [XXX 註冊系統]！");
+            Console.WriteLine("請依照指示完成註冊，讓我們開始吧！");
+            
+            Console.WriteLine("請輸入您的姓名：");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("接下來，請輸入您的帳號：");
+            string account = Console.ReadLine();
+
+            Console.WriteLine("請設置一個安全的密碼：");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("最後，請輸入您的聯絡地址，讓我們方便與您保持聯繫：");
+            string address = Console.ReadLine();
+
         }
     }
 }
